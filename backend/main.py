ecc0e8554417c57c0fdb98e92be8935a9720641f
googol88 <ronaldfleung@gmail.com>
2022-07-08 12:06:05 -0400

    fake code for ifttt request

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,22 @@
 import requests
+import argparse
 
-requests.post('https://connect.ifttt.com')
+def get_arguments():
+    ap = argparse.ArgumentParser()
+    ap.add_argument('-b', '--brightness', required=True,
+                    help='Brightness of the light bulb (20, 50, 75, 100)')
+    args = vars(ap.parse_args())
+    return args
+
+def main():
+  args = get_arguments()
+
+  if args['brightness'] & int(args['brightness']) in [20, 50, 75, 100]:
+    r = requests.get(f'https://maker.ifttt.com/trigger/set{args.brightness}/json/with/key/dJ4Od62pIDkyxQsDFCmLzkZfiEbSpJxbq_m00iXHuc-')
+    print(r.status_code)
+
+  else:
+    print('Invalid brightness value')
+
+if __name__ == '__main__': 
+    main()
